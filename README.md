ff78606fb200aebe7f87be42db83c1d2992a3f3a
DadoIII <daniel.zelinka9@gmail.com>
2023-07-18 20:19:57 +0100

    Added solutions for day 12

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # AdventOfCode2022
-My solutions for Advent of Code 2022
+My solutions for Advent of Code 2022. Most of the files only cointain the solution for part 2.
---
+++ day12_part1.py
@@ -0,0 +1,81 @@
+import copy
+
+class Node():
+    def __init__(self, height, visited = False):
+        self.height = height
+        self.visited = visited
+
+f = open('input12.txt', 'r')
+
+grid = []
+
+line = f.readline().strip()
+
+visited_nodes = []
+row =  0
+
+
+while line != '':
+    inner_grid = []
+    
+    col = 0
+    for character in line:
+        if ord(character) >= 97 and ord(character) <= 122:
+            inner_grid.append(Node(ord(character) - 96))
+        elif character == 'S':
+            inner_grid.append(Node(height = 0, visited = True))
+            visited_nodes.append((row, col))
+        elif character == 'E':
+            inner_grid.append(Node(27))
+        col += 1
+
+    row += 1
+    grid.append(inner_grid)
+    line = f.readline().strip()
+
+
+count = 0
+end = False
+while not end:
+    to_visit = []
+
+    print(visited_nodes)
+    for (y, x) in visited_nodes:
+        height = grid[y][x].height
+
+        #check if we reached the end
+        if height == 27:
+            print(count)
+            end = True
+            break
+
+        #go left
+        if x - 1 >= 0 and not grid[y][x-1].visited and grid[y][x-1].height - 1 <= height:
+            to_visit.append((y, x-1))
+            grid[y][x-1].visited = True
+        
+        #go right
+        if x + 1 < len(grid[0]) and not grid[y][x+1].visited and grid[y][x+1].height - 1 <= height:
+            to_visit.append((y, x+1))
+            grid[y][x+1].visited = True
+
+        #go up
+        if y - 1 >= 0 and not grid[y-1][x].visited and grid[y-1][x].height - 1 <= height:
+            to_visit.append((y-1, x))
+            grid[y-1][x].visited = True
+
+        #go down
+        if y + 1 < len(grid) and not grid[y+1][x].visited and grid[y+1][x].height - 1 <= height:
+            to_visit.append((y+1, x))
+            grid[y+1][x].visited = True
+    
+    
+    visited_nodes = copy.copy(to_visit)
+    if len(visited_nodes) == 0:
+        end = True
+    count += 1
+        
+
+
+
+
